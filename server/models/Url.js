617e8f3593c4da707648de3a2e66d7f1d8678da3
wmezadev <williameduardomeza@gmail.com>
2023-07-14 13:20:16 -0500

    feature: update Url model to support timestamp

--- server/models/Url.js
+++ server/models/Url.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const urlSchema = new mongoose.Schema({
+const urlSchema = new mongoose.Schema(
+  {
     id: { type: String, required: true },
     originalUrl: { type: String, required: true },
     shortUrl: String,
     clicks: {
-        type: Number,
-        required: true,
-        default: 0
-    },
-    date: {
-        type: String,
-        default: Date.now
+      type: Number,
+      required: true,
+      default: 0,
     }
-})
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Url', urlSchema);
+module.exports = mongoose.model("Url", urlSchema);
