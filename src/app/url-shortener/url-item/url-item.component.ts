35c577ec63ef4c657fc643ad5fb151b559215a95
wmezadev <williameduardomeza@gmail.com>
2023-07-15 01:21:48 -0500

    feat: Retreive URL with clicks data

--- src/app/url-shortener/url-item/url-item.component.ts
+++ src/app/url-shortener/url-item/url-item.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UrlShortenerService } from '../url-shortener.service';
+import { Url } from '../Url.model';
 @Component({
   selector: 'app-url-item',
   templateUrl: './url-item.component.html',
   styleUrls: ['./url-item.component.css'],
 })
-export class UrlItemComponent {}
+export class UrlItemComponent implements OnInit {
+  url!: Url | null;
+  constructor(
+    private urlShortenerService: UrlShortenerService,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(async (params) => {
+      const id = params['id'];
+      this.url = await this.urlShortenerService.getUrl(id);
+      console.log(this.url);
+    });
+  }
+}
