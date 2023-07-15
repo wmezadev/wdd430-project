0c5284e2559912b8daf572f799363f7375a0f506
wmezadev <williameduardomeza@gmail.com>
2023-07-14 20:24:13 -0500

    feat: Creates init url-list component to use as Dashboard

---
+++ src/app/url-shortener/url-list/url-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-url-list',
+  templateUrl: './url-list.component.html',
+  styleUrls: ['./url-list.component.css']
+})
+export class UrlListComponent {
+
+}
