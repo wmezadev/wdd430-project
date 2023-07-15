8fd77a07ece78bf95edfbd7ecab2670f4e7c0a1a
wmezadev <williameduardomeza@gmail.com>
2023-07-14 22:24:59 -0500

    feat: Adds url client side validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UrlCreateComponent } from './url-shortener/url-create/url-create.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { UrlListComponent } from './url-shortener/url-list/url-list.component';
 
 @NgModule({
   declarations: [AppComponent, UrlCreateComponent, HeaderComponent, UrlListComponent],
-  imports: [AppRoutingModule, BrowserModule, FormsModule, HttpClientModule],
+  imports: [AppRoutingModule, BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/url-shortener/url-create/url-create.component.ts
+++ src/app/url-shortener/url-create/url-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { UrlShortenerService } from '../url-shortener.service';
 import { Subscription } from 'rxjs';
 import { Url } from '../Url.model';
@@ -7,16 +8,23 @@ import { Url } from '../Url.model';
   templateUrl: './url-create.component.html',
   styleUrls: ['./url-create.component.css'],
 })
-export class UrlCreateComponent {
-  fullUrl!: string;
+export class UrlCreateComponent implements OnInit {
+  longUrl!: string;
   shortenedUrl!: string;
   subscription: Subscription = new Subscription();
+  myForm!: FormGroup;
 
-  constructor(private urlService: UrlShortenerService) {}
+  constructor(
+    private urlService: UrlShortenerService,
+    private formBuilder: FormBuilder,
+  ) {}
 
   ngOnInit() {
     this.subscription = this.urlService.lastUrlChangedEvent.subscribe((url: Url) => {
       this.shortenedUrl = url.shortUrl;
+    });
+    this.myForm = this.formBuilder.group({
+      longUrl: new FormControl('', [Validators.required, this.validateLink]),
     });
   }
 
@@ -24,9 +32,41 @@ export class UrlCreateComponent {
     this.subscription.unsubscribe();
   }
 
+  isFieldInvalid(fieldName: string): boolean {
+    const field = this.myForm.get(fieldName);
+    return Boolean(field?.invalid && (field?.dirty || field?.touched));
+  }
+
+  getValidationMessage(fieldName: string): string {
+    const field = this.myForm.get(fieldName);
+
+    if (field?.hasError('required')) {
+      return 'This field is required';
+    }
+    if (field?.hasError('invalidLink')) {
+      return 'Invalid link format';
+    }
+
+    return '';
+  }
+
+  validateLink(control: any): { [key: string]: boolean } | null {
+    const linkPattern = /^(http|https):\/\/[^ "]+$/;
+
+    if (control.value && !linkPattern.test(control.value)) {
+      return { invalidLink: true };
+    }
+
+    return null;
+  }
+
   submitForm() {
-    // Here, you can implement the logic to submit the full URL and get the shortened URL
-    // This is just a placeholder code
-    this.urlService.shortenUrl(this.fullUrl);
+    if (this.myForm.valid) {
+      // Submit longUrl
+      this.urlService.shortenUrl(this.myForm.value.longUrl);
+    } else {
+      // Mark fields as touched to trigger validation messages
+      this.myForm.markAllAsTouched();
+    }
   }
 }
